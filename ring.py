72698a71001fe8472787d4a655e19da4e8dd55f3
nataniel.parente@aluno.uece.net <natanielpn@gmail.com>
2020-02-17 14:46:46 -0300

    r

--- ring.py
+++ ring.py
@@ -1,4 +1,7 @@
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
 
 class Ring( Topo ):
     "Ring topology example."
